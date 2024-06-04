92e45af296e3a7601b913815c31328670b23dfc8
Austin Bond <austinbond@Austins-MacBook-Air-4.local>
2024-06-04 18:17:52 -0400

    changed rollup config and package.json

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import packageJson from './package.json' assert { type: 'json' };
 
 export default [
   {
-    input: 'src/index.ts',
+    input: 'src/components/index.tsx',
     output: [
       {
         file: packageJson.main,
