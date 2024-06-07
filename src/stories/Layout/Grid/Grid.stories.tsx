1e8aebbe7f3640ee138940a94eb47e2f2dbe91e5
Austin Bond <austinbond@Austins-MacBook-Air-4.local>
2024-06-06 21:00:30 -0400

    changes to grid css

--- src/stories/Layout/Grid/Grid.stories.tsx
+++ src/stories/Layout/Grid/Grid.stories.tsx
@@ -22,14 +22,14 @@ type Story = StoryObj<typeof meta>;
 
 const gridChildren = (
   <>
-    <div className="col-12 col-s-12" style={{ backgroundColor: 'blue', padding: '5rem'}}>
+    <div className="col-container" style={{ backgroundColor: 'blue', padding: '3rem'}}>
       <div className="col-3 col-s-3" style={{ backgroundColor: '#f0f0f0'}}>Item 1</div>
       <div className="col-3 col-s-3" style={{ backgroundColor: '#c0c0c0' }}>Item 2</div>
       <div className="col-3 col-s-3" style={{ backgroundColor: '#f0f0f0', }}>Item 3</div>
       <div className="col-3 col-s-3" style={{ backgroundColor: '#c0c0c0', }}>
-        <div className="col-12 col-s-12" style={{ backgroundColor: 'black'}}> 
-        <div className="col-6 col-s-6" style={{ backgroundColor: '#f0f0f0' }}>Item 4</div>
-        <div className="col-6 col-s-6" style={{ backgroundColor: '#f0f0f0' }}>Item 5</div>
+        <div className="col-container" style={{ backgroundColor: 'black'}}> 
+          <div className="col-6 col-s-6" style={{ backgroundColor: '#f0f0f0' }}>Item 4</div>
+          <div className="col-6 col-s-6" style={{ backgroundColor: '#f0f0f0' }}>Item 5</div>
         </div>
       </div>
     </div>
